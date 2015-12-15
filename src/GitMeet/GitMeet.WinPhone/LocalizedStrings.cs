f7983aa7935fec51ac6935be1a1f9cdfead49d9f
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2015-12-15 11:45:47 -0500

    Initial commit for GitMeet project

---
+++ src/GitMeet/GitMeet.WinPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GitMeet.WinPhone.Resources;
+
+namespace GitMeet.WinPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
