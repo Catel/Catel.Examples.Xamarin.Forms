e0f10e3e01ca1c9f09dedc4a5e2abc7b6a9ebfdc
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2015-12-22 16:05:27 -0500

    Added missing file

---
+++ src/GitMeet/GitMeet/Models/RoomInfo.cs
@@ -0,0 +1,41 @@
+﻿using Catel.Data;
+using Newtonsoft.Json;
+
+namespace GitMeet.Models
+{
+    /// <summary>
+    ///     The room info/
+    /// </summary>
+    public sealed class RoomInfo : ModelBase
+    {
+        /// <summary>Register the Name property so it is known in the class.</summary>
+        public static readonly PropertyData NameProperty = RegisterProperty<RoomInfo, string>(model => model.Name);
+
+        /// <summary>Register the UnreadItems property so it is known in the class.</summary>
+        public static readonly PropertyData UnreadItemsProperty = RegisterProperty<RoomInfo, int>(model => model.UnreadItems);
+
+        /// <summary>Register the Id property so it is known in the class.</summary>
+        public static readonly PropertyData IdProperty = RegisterProperty<RoomInfo, string>(model => model.Id);
+
+        [JsonProperty("id")]
+        public string Id
+        {
+            get { return GetValue<string>(IdProperty); }
+            set { SetValue(IdProperty, value); }
+        }
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get { return GetValue<string>(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        [JsonProperty("unreadItems")]
+        public int UnreadItems
+        {
+            get { return GetValue<int>(UnreadItemsProperty); }
+            set { SetValue(UnreadItemsProperty, value); }
+        }
+    }
+}
