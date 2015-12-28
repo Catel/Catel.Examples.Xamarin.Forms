11bc26444398787115b13fe142aedd0d055f77cc
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2015-12-28 16:41:33 -0500

    Fixed GitHub login page url

--- src/GitMeet/AuthorizationFlowWellKnownUrls.cs
+++ src/GitMeet/AuthorizationFlowWellKnownUrls.cs
@@ -15,7 +15,7 @@ namespace GitMeet
         /// <summary>
         /// The github login page request url.
         /// </summary>
-        public const string GitHubLoginRequestPageUrl = "https://github.com/login?return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3De865e74c1d0a3e66003c%26redirect_uri%3Dhttps%253A%252F%252Fgitter.im%252Flogin%252Fcallback%26response_type%3Dcode%26scope%3Duser%253Aemail%252Cread%253Aorg%26state%3D9bff6fdc2b0f2f3188676f70fe2a5316a28518fca75e2af0e6b5ddb0b6a08b34";
+        public const string GitHubLoginRequestPageUrl = "https://github.com/login";
 
         /// <summary>
         /// The github home page url.
