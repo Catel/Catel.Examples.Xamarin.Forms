1b9514cbed9ca5158d4b173edee025867aeb158b
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2016-01-04 16:59:07 -0500

    List in the the main page rooms with it's avatar and the amount unread Items

--- src/GitMeet/Models/RoomInfo.cs
+++ src/GitMeet/Models/RoomInfo.cs
@@ -1,21 +1,21 @@
-﻿using Catel.Data;
-using Newtonsoft.Json;
-
-namespace GitMeet.Models
+﻿namespace GitMeet.Models
 {
+    using Catel.Data;
+    using Newtonsoft.Json;
+
     /// <summary>
     ///     The room info/
     /// </summary>
     public sealed class RoomInfo : ModelBase
     {
-        /// <summary>Register the Name property so it is known in the class.</summary>
-        public static readonly PropertyData NameProperty = RegisterProperty<RoomInfo, string>(model => model.Name);
-
         /// <summary>Register the UnreadItems property so it is known in the class.</summary>
         public static readonly PropertyData UnreadItemsProperty = RegisterProperty<RoomInfo, int>(model => model.UnreadItems);
 
         /// <summary>Register the Id property so it is known in the class.</summary>
         public static readonly PropertyData IdProperty = RegisterProperty<RoomInfo, string>(model => model.Id);
+
+        /// <summary>Register the Name property so it is known in the class.</summary>
+        public static readonly PropertyData NameProperty = RegisterProperty<RoomInfo, string>(model => model.Name, default(string), (s, e) => s.OnNameChanged(e));
 
         [JsonProperty("id")]
         public string Id
@@ -36,6 +36,18 @@ namespace GitMeet.Models
         {
             get { return GetValue<int>(UnreadItemsProperty); }
             set { SetValue(UnreadItemsProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets the avatar url.
+        /// </summary>
+        public string AvatarUrl => "https://avatars2.githubusercontent.com/" + Name.Split('/')[0];
+
+        /// <summary>Occurs when the value of the Name property is changed.</summary>
+        /// <param name="e">The event argument</param>
+        private void OnNameChanged(AdvancedPropertyChangedEventArgs e)
+        {
+            RaisePropertyChanged(nameof(AvatarUrl));
         }
     }
 }
